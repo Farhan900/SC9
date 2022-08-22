3691aed7909904d82537539864c39f2031736f95
Farhan900 <109672555+Farhan900@users.noreply.github.com>
2022-08-23 01:29:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ global.mess = {
     group: 'Fitur Digunakan Hanya Untuk Group!',
     private: 'Fitur Digunakan Hanya Untuk Private Chat!',
     bot: 'Fitur Khusus Pengguna Nomor Bot',
-    wait: 'Loading...',
+    wait: 'tunggu 1 tahun lagi...',
     endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
 }
 global.limitawal = {
